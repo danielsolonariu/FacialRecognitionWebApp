ef732e3c31a2c4f35675c8be15691d619bb49294
Daniel Solonariu <daniel.solonariu.dev@gmail.com>
2023-07-21 15:18:48 +0300

    Register has been created. Connected with MongoDB

--- flask/sv.py
+++ flask/sv.py
@@ -12,13 +12,50 @@ MONGODB_URI = f"mongodb+srv://admin:{password}@personalprojects.enxjsep.mongodb.
 client = MongoClient(MONGODB_URI)
 database = client.FacialRecognitionWebApp
 
+db_users = database.users
+
 app = Flask(__name__)
 app.secret_key = os.environ.get("APP_SECRET_KEY")
 
-@app.route("/index")
-def home():
-    return render_template("app/index.html")
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("app/index.html")
+    
+    username = request.form.get("username")
+    password = request.form.get("password")
 
+    passwordhash = sha256(password.encode()).hexdigest()
+
+    user_doc = db_users.find_one({"username": username})
+
+    if user_doc is None or user_doc["password"] != passwordhash:
+        return render_template("index.html", error="Wrong username or password. Try again or click \"Forgot your password?\" to reset it.")
+
+    session["username"] = username
+
+    session["is_admin"] = user_doc.get("is_admin", False)
+
+    return redirect("/dashboard")
+
+@app.route("/register", methods=["GET", "POST"])
+def sign_up():
+    if request.method == "GET":
+        return render_template("auth/register.html")
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+    repeat_password = request.form.get("repeat_password")
+
+    if db_users.find_one({"username": username}):
+        return render_template("auth/register.html", error="Username already exist. Please choose another username.", username=username, password=password, repeat_password=repeat_password)
+
+    if password != repeat_password:
+        return render_template("auth/register.html", error="Passwords do not match. Please try again.", username=username, password=password, repeat_password=repeat_password)
+
+    db_users.insert_one({"username": username, "password": sha256(password.encode()).hexdigest()})
+
+    return redirect("/")
 
 
 app.run("0.0.0.0", port=80, debug=True)
